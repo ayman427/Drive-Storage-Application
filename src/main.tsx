f1dbc107192b70e4099ecb1ada9c4169870bc270
ayman427 <aymankarovadiya1@gmail.com>
2025-02-16 14:24:44 +0530

    clerk api key changed

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,15 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env.local file");
+}
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey="pk_test_cHJpbWUtbGFiLTY1LmNsZXJrLmFjY291bnRzLmRldiQ">
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         <App />
       </ConvexProviderWithClerk>
